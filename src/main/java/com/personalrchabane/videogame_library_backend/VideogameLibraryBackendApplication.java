ee5c8265a854275a629bd9839c636c3009c1b119
Rachid Chabane <rachid.chabane59@gmail.com>
2024-12-23 10:12:59 +0100

    Initialized backend

---
+++ src/main/java/com/personalrchabane/videogame_library_backend/VideogameLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.personalrchabane.videogame_library_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogameLibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogameLibraryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personalrchabane/videogame_library_backend/VideogameLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalrchabane.videogame_library_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogameLibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
