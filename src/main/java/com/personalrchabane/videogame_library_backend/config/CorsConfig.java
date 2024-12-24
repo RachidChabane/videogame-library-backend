2e87a41bda70ff27cac31847f6c13c02b1510bf8
Rachid Chabane <rachid.chabane59@gmail.com>
2024-12-24 09:51:19 +0100

    Dynamisation du CORS

--- src/main/java/com/personalrchabane/videogame_library_backend/config/CorsConfig.java
+++ src/main/java/com/personalrchabane/videogame_library_backend/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.personalrchabane.videogame_library_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,13 +13,29 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
+    @Value("${cors.allowed-methods}")
+    private String allowedMethods;
+
+    @Value("${cors.allowed-headers}")
+    private String allowedHeaders;
+
+    @Value("${cors.exposed-headers}")
+    private String exposedHeaders;
+
+    private List<String> getListFromPropertiesString(String value) {
+        return Arrays.asList(value.split(","));
+    }
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
-        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+        corsConfiguration.setAllowedOrigins(getListFromPropertiesString(allowedOrigins));
+        corsConfiguration.setAllowedMethods(getListFromPropertiesString(allowedMethods));
+        corsConfiguration.setAllowedHeaders(getListFromPropertiesString(allowedHeaders));
+        corsConfiguration.setExposedHeaders(getListFromPropertiesString(exposedHeaders));
         corsConfiguration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
